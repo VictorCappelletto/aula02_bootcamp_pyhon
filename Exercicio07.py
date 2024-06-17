62f4d75d3a40f41656c778a2f55cb58126e9abf4
victor.castro <victorcappelletto@gmail.com>
2024-06-17 20:09:32 -0300

    alteração exercicio 07

--- Exercicio07.py
+++ Exercicio07.py
@@ -3,6 +3,5 @@ import statistics
 
 numero_1=float(input("insira o primeiro numero: "))
 numero_2=float(input("insira o segundo numero : "))
-#soma=[numero_1+numero_2]
 media=statistics.mean([numero_1,numero_2])
 print(f'Olá usuário, a média entre os valores é {media:.2f}')
