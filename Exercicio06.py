41f3a3e27324cbb2b667987a5e895d6a4e6c6215
victor.castro <victorcappelletto@gmail.com>
2024-06-17 20:07:43 -0300

    alteração exercicio 6

---
+++ Exercicio06.py
@@ -0,0 +1,5 @@
+#Exercicio: Escreva um programa que receba dois números flutuantes e realize sua adição.
+numero_1=float(input("insira o primeiro numero com duas casas decimais: "))
+numero_2=float(input("insira o segundo numero com duas casas decimais: "))
+soma=numero_1 +numero_2
+print(f'O resultado da soma é {soma:.2f}')
