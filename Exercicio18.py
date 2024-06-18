3794e7611a7fe4aedc90d5c9923176059740ea0d
victor.castro <victorcappelletto@gmail.com>
2024-06-18 18:28:20 -0300

    add exercicio 18

---
+++ Exercicio18.py
@@ -0,0 +1,6 @@
+#Exercicio: Desenvolva um programa que peça ao usuário para inserir um valor booleano e, 
+# em seguida, inverta esse valor.
+
+condicao_1=bool(input("insira um novo nome "))
+cond_final= not condicao_1 
+print("Condição de acesso do cliente: ",cond_final)
