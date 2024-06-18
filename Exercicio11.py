d60379c345b65925dcdaf1ebe72051b54b7cf6c8
victor.castro <victorcappelletto@gmail.com>
2024-06-17 23:20:07 -0300

    add exercicio 11

---
+++ Exercicio11.py
@@ -0,0 +1,5 @@
+#Exercicio: Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
+palavra=input("insira uma palavra: ")
+palavra_maiusc=str(palavra.upper())
+
+print(f'Sua palavra maiúsculas: {palavra_maiusc}')
