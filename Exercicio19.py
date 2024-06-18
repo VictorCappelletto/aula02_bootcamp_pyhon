5ea252a0605bf1a9ef2ee33714f5549ea48e68e2
victor.castro <victorcappelletto@gmail.com>
2024-06-18 18:28:33 -0300

    add exercicio 19

---
+++ Exercicio19.py
@@ -0,0 +1,5 @@
+#Exercicio: Faça um programa que compare se dois números fornecidos pelo usuário são iguais.
+condicao_1=int(input("insira um numero "))
+condicao_2=int(input("insira outro numero "))
+cond_final= bool(condicao_1==condicao_2) 
+print("Condição de igualdade dos numeros: ",cond_final)
