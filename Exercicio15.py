2da55a1471c929e3878a4b35ae33536ef973b774
victor.castro <victorcappelletto@gmail.com>
2024-06-17 23:21:41 -0300

    add exercicio 15

---
+++ Exercicio15.py
@@ -0,0 +1,5 @@
+#Exercicio: Escreva um programa que concatene duas strings fornecidas pelo usuário.
+palavra_1=input("insira a primeira palavra: ")
+palavra_2=input("insira a segunda palavra: ")
+soma_palavra=palavra_1+palavra_2
+print(f'União das duas palavras em uma: {soma_palavra}')
