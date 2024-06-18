1d3a8b542dfd2b483cb5bf519c54b412c9e122e6
victor.castro <victorcappelletto@gmail.com>
2024-06-18 18:28:44 -0300

    add exercicio 20

---
+++ Exercicio20.py
@@ -0,0 +1,5 @@
+#Exercicio: Escreva um programa que verifique se dois números fornecidos pelo usuário são diferentes.
+condicao_1=int(input("insira um numero "))
+condicao_2=int(input("insira outro numero "))
+cond_final= bool(condicao_1!=condicao_2) 
+print("Condição de igualdade dos numeros: ",cond_final)
