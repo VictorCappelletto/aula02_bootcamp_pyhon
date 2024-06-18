0d0a074f028c6b44c0b97fdcb85a99f5696d29b1
victor.castro <victorcappelletto@gmail.com>
2024-06-18 18:27:44 -0300

    add exercicio 13

--- Exercicio13.py
+++ Exercicio13.py
@@ -2,5 +2,5 @@
 # em seguida, imprima esta frase sem espaços em branco no início e no final.
 
 frase_user=(input("insira uma frase: "))
-frase_sem_espaco=frase_user.strip(" ")
+frase_sem_espaco=frase_user.strip()
 print(f'Sua frase sem espaços: {frase_sem_espaco}')
